6858b8a457759f6e06c841531f7c2515cb90f37d
Hakyur <drogotovsky@gmail.com>
2025-01-18 22:16:59 +0300

    first commit

---
+++ src/main/java/ru/rogotovskiy/game_library_api/GameLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package ru.rogotovskiy.game_library_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rogotovskiy/game_library_api/GameLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rogotovskiy.game_library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
